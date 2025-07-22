8a6fd3f6cc57652030c94f915f1e6da7f4a97e71
benjaminkle <benjamin.kle@hotmail.com>
2025-07-21 20:52:56 -0400

    js for mouse and keyboard events

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //Mouse Events Sections
+const mouseBtn = document.getElementById("mouseBtn");
+const mouseMsg = document.getElementById("mouseMsg");
+
+mouseBtn.addEventListener("mouseenter", () => {
+  mouseMsg.textContent = "Hovering over the button!";
+});
+mouseBtn.addEventListener("mouseleave", () => {
+  mouseMsg.textContent = "Unselected the button!";
+});
 //Keyboard Events Section
+const keyInput = document.getElementById("keyInput");
+const keyMsg = document.getElementById("keyMsg");
+
+keyInput.addEventListener("keydown", (event) => {
+  keyMsg.textContent = `Last key pressed: ${event.key}`;
+});
+
 //Form Events Section
 //Focus and Blur Events Section
 //Event Degelations Section
